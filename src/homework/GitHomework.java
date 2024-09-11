eb6d122852f9f35d3b1cf194d1e90ad19bb765b7
luis <luis@DESKTOP-NHDHS8H>
2024-09-10 17:39:16 -0700

    added method

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -9,4 +9,8 @@ public class GitHomework {
 	public static void add(int a, int b) {
 		System.out.println(a + b);
 	}
+	
+	public static void subtract(int a, int b) {
+		System.out.println(a - b);
+	}
 }
