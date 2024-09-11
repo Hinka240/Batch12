cf85b5d6266babefc61126b0c41020df8af73fb4
Hinka240 <kayla.d.trejo08@gmail.com>
2024-09-10 17:50:15 -0700

    Update GitHomework.java

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -3,6 +3,9 @@ package homework;
 public class GitHomework {
 	public static void main(String[] args) {
 		System.out.println("BATCH 12: Git Class Lecture");
+		int age = 12;
+		String name = "John";
+		// This is coming from github
 
 	}
 
