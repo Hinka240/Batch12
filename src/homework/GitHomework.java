53b68fa2cc0281fdd436387bf2ad85b8cd2f4337
luis <luis@DESKTOP-NHDHS8H>
2024-09-10 19:06:19 -0700

    removed code

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -2,13 +2,5 @@ package homework;
 
 public class GitHomework {
 	
-	public void display() {
-		System.out.println("Kayla T");
-	}
-	/**
- this is coming from github
- java is fun
- github is easy
- api is not fun 
-	*/
+	
 }
