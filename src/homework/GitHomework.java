419da04fb5ec9b1114b03483cb00395771ee17c6
Hinka240 <kayla.d.trejo08@gmail.com>
2024-09-10 18:53:22 -0700

    Update GitHomework.java

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -5,5 +5,10 @@ public class GitHomework {
 	public void display() {
 		System.out.println("Kayla T");
 	}
-	
+	/**
+ this is coming from github
+ java is fun
+ github is easy
+ api is not fun 
+	*/
 }
